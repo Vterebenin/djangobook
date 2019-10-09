d8e0ad8bd2e2eab31cc9b82bc9e3a79efd88541e
Vterebenin <niceserver11@mail.ru>
2019-10-09 23:22:09 +0300

    almost finish first tutorial

--- mfdw_site/pages/views.py
+++ mfdw_site/pages/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>The Meandco Homepage</h1>")
-
+    # return HttpResponse("<h1>The Meandco Homepage</h1>")
+    return render(request, 'pages/page.html')
 # Create your views here.
